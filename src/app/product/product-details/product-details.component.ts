0495ab3e40c3828686fa3f3d43269748157042fc
kKapalka <k.kapalka@hotmail.com>
2021-08-15 19:27:43 +0200

    product-service + wyświetlanie danych produktu w detailsach

--- src/app/product/product-details/product-details.component.ts
+++ src/app/product/product-details/product-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {ProdList} from "../../prod-list";
+import {ProductService} from "../../product.service";
 
 @Component({
   selector: 'app-product-details',
@@ -9,16 +11,20 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductDetailsComponent implements OnInit {
 
   productId: number | undefined;
-
-  constructor(private route: ActivatedRoute) { }
+  product: ProdList | undefined;
+  constructor(private route: ActivatedRoute, private productService: ProductService) { }
 
   ngOnInit(): void {
     this.loadedProduct();
 
-    
   }
 
   loadedProduct() {
     this.productId = +this.route.snapshot.params['id'];
+    this.product = this.productService.productList.find(el => el.id === this.productId);
+  }
+
+  getProduct() {
+    return JSON.stringify(this.product);
   }
 }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ProdList } from '../prod-list';
+import {ProductService} from "../product.service";
 
 @Component({
   selector: 'app-product',
@@ -11,55 +12,8 @@ export class ProductComponent implements OnInit {
 
   productList: ProdList[];
 
-  constructor() { 
-    this.productList = [
-      {
-        'id': 1,
-        'unit': 1,
-        'weight': 100,
-        'name': 'Jajko',
-        'carbohydrates': 50,
-        'proteins': 20,
-        'fats':30
-      },
-      {
-        'id': 2,
-        'unit': 1,
-        'weight': 100,
-        'name': 'Chleb razowy',
-        'carbohydrates': 130,
-        'proteins': 4,
-        'fats': 30
-      },
-      {
-        'id': 3,
-        'unit': 1,
-        'weight': 100,
-        'name': 'Jabłko',
-        'carbohydrates': 150,
-        'proteins': 4,
-        'fats': 3
-      },
-      {
-        'id': 4,
-        'unit': 1,
-        'weight': 100,
-        'name': 'Ziemniaki',
-        'carbohydrates': 50,
-        'proteins': 8,
-        'fats': 3
-      },
-      {
-        'id': 5,
-        'unit': 1,
-        'weight': 100,
-        'name': 'Wołowina',
-        'carbohydrates': 250,
-        'proteins': 17,
-        'fats': 36
-      }
-    ]
-
+  constructor(private productService: ProductService) {
+    this.productList = productService.productList;
   }
 
   ngOnInit() {
